c94dc11bf636132f76ce2687d8ad4dc300ecadcb
Youssef Mohammed <yoseffmohamed001@gmail.com>
2023-12-12 01:57:31 +0200

    chat screen

--- lib/features/chats/view/screens/chats_screen.dart
+++ lib/features/chats/view/screens/chats_screen.dart
@@ -1,10 +1,45 @@
+import 'package:bekya/core/styles/colors.dart';
+import 'package:bekya/features/chats/view/widgets/chat_item.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class ChatsScreen extends StatelessWidget {
   const ChatsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.second,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Container(
+          margin: EdgeInsets.all(13.sp),
+          child: Text(
+            "Chats",
+            style: TextStyle(
+                color: AppColors.offWhite,
+                fontSize: 18.sp,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        Expanded(
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return const ChatItem(image: 'Y', name: 'Menna', message: 'I LOVE YOU', date: '24/11',);
+            },
+            itemCount: 12, separatorBuilder: (BuildContext context, int index) {
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 20.sp),
+                child: Divider(
+                thickness: 1.sp,
+                color: Colors.grey[200],
+                ),
+              );
+              },
+          ),
+        ),
+      ],),
+    );
   }
 }
